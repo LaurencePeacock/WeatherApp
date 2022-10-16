835465e2a5f20f9903ccecca4ee121d1ddb1aa5d
Laurence Peacock <laurencepeacock@hotmail.com>
2022-10-16 13:25:36 +0100

    chance of precipitation added

--- src/App.js
+++ src/App.js
@@ -9,18 +9,29 @@ import Forecasts from './Components/Forecasts';
 import LocationHeader from './Components/LocationHeader';
 import './css/weather-icons-wind.css';
 import './css/weather-icons.css';
+// import BeatLoader from 'react-spinners/BeatLoader';
+import { HashLoader } from 'react-spinners';
 
 function App() {
   
+  const [loading, setLoading] = useState(false);
+
   const [openWeatherData, setOpenWeatherData] = useState();
+
   useEffect(() => {
+    setLoading(true);
     const fetchWeather = async () => {
+      setTimeout(()=> {
         axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=53.91&lon=-1.74&appid=${config.key}`)
         .then((res) => setOpenWeatherData(res.data))
         .catch((err) => console.log(err));
+        setLoading(false);
+      },1000)
     };
     fetchWeather();
+    
 }, [])
+
    
   const [forecasts, setForecasts] = useState();
   
@@ -40,14 +51,24 @@ function App() {
         }, 10000);
         return () => clearInterval(interval);
       }, []);
+
+  const override: CSSProperties = {
+        display: "block",
+        margin: "3rem auto",
+        height: "26vh",
+      };
+    const color = "#c424d6";  
   
   return (
-    <div>
-        {  currentDay && currentHour && currentMinute && <DateTimeBanner currentDay={currentDay} currentHour={currentHour} currentMinute={currentMinute}/>}
-        {openWeatherData && <LocationHeader weather={openWeatherData}/>}
-        { openWeatherData && <CurrentWeather weather={openWeatherData}/>}
-        { openWeatherData && <Forecasts forecasts={openWeatherData}/>}
-    </div>
+     
+      <div>
+          { currentDay && currentHour && currentMinute && <DateTimeBanner currentDay={currentDay} currentHour={currentHour} currentMinute={currentMinute}/>}
+          { openWeatherData && <LocationHeader weather={openWeatherData}/>}
+          { loading ? <HashLoader cssOverride={override} color={color}/> : openWeatherData && <CurrentWeather weather={openWeatherData}/>}
+          { loading ? <HashLoader cssOverride={override}/> : openWeatherData && <Forecasts forecasts={openWeatherData}/>}
+      </div>
+    
+    
   );
 }
 
--- src/Components/HourlyForecasts.jsx
+++ src/Components/HourlyForecasts.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-
+import dayjs from 'dayjs';
 
 function HourlyForecasts( {hourForecast, forecastIndex} ) {
     
-    const date = new Date(hourForecast.dt*1000)  
-     if(forecastIndex % 2 == 1){
+    const date = new Date(hourForecast.dt*1000) 
+    const weekday = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+    const day = weekday[date.getDay()];
+
+     if(forecastIndex % 2 === 1){
     return (
         <div className='hourly-forecast'>
-            {/* <div className="forecast-day">{date.getDay()}</div> */}
+            <div className="forecast-day">{day}</div>
             <div className='forecast-time'><b>{date.getHours()}</b>:00</div>
             <div><img className="hour-forecast-weather-icon" src={`https://openweathermap.org/img/wn/${hourForecast.weather[0].icon}@2x.png`}/></div>
             <div className="hour-forecast-temp">{Math.round((hourForecast.feels_like - 273.15))}<i className='wi wi-celsius'></i></div>
             <div className="hour-forecast-wind">{Math.round((hourForecast.wind_speed*2.237))}<span className='mph'> mph</span></div>
-            {/* <div className="hour-forecast-pop"> {hourForecast.pop}<i className='wi wi-humidity'></i></div> */}
+            <div className="hour-forecast-pop"><div>{hourForecast.pop}</div> <i className='pop-icon'> </i></div>
         </div>  
     )
     }
 }
-
+ 
 export default HourlyForecasts
